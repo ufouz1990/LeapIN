a486c8ab102db0c120a606fb8e8d8cd3cf2af62e
Peter Wright <peter.wright94@gmail.com>
2014-02-08 14:07:13 +0000

    Mouse cursor can be moved

--- MasterProject/LeapIN/LeapIN/LeapControl.cs
+++ MasterProject/LeapIN/LeapIN/LeapControl.cs
@@ -3,6 +3,10 @@ using Leap;
 
 namespace LeapIN
 {
+    /// <summary>
+    /// This class could be used to implement a listener to call methods
+    /// such as OnConnect, OnFrame, OnExit and so on
+    /// </summary>
     class LeapControl : Listener
     {
 
--- MasterProject/LeapIN/LeapIN/MainInterface.cs
+++ MasterProject/LeapIN/LeapIN/MainInterface.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Ink;
+
+using Leap;
+using System.Runtime.InteropServices;
+
 
 namespace LeapIN
 {
@@ -12,6 +20,45 @@ namespace LeapIN
     /// </summary>
     class MainInterface
     {
+        Controller leap;
+        double windowWidth;
+        double windowHeight;
 
+        public MainInterface()
+        {
+            // Set up the cursor
+            leap = new Controller();
+
+            // Get the window size
+            windowWidth = SystemParameters.PrimaryScreenWidth;
+            windowHeight = SystemParameters.PrimaryScreenHeight;
+
+            // Add event to update frame with pointer position
+            CompositionTarget.Rendering += UpdateFrame;
+        }
+
+        // Required system win32 API call to move the cursor
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        protected void UpdateFrame(object sender, EventArgs e)
+        {
+            // Grab the frame and the nearest pointer
+            Leap.Frame frame = leap.Frame();
+            Pointable pointable = frame.Pointables.Frontmost;
+            Leap.Vector stabilizedPos = pointable.StabilizedTipPosition;
+
+            // Calculate the position in screen space
+            InteractionBox iBox = leap.Frame().InteractionBox;
+            Leap.Vector normalizedPosition = iBox.NormalizePoint(stabilizedPos);
+            double tx = normalizedPosition.x * windowWidth;
+            double ty = windowHeight - normalizedPosition.y * windowHeight;
+
+            // Allows use of mouse normally
+            if (pointable.TouchDistance > 0 && pointable.TouchZone != Pointable.Zone.ZONENONE)
+            {
+                SetCursorPos((int)tx, (int)ty);
+            }
+        }
     }
 }
